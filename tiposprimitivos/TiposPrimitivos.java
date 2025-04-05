c568838667c97f17e83afccebf4a10cf7fb83aff
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 20:24:56 -0300

    adicionando apenas 2 casas decimais

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -5,6 +5,6 @@ import java.sql.SQLOutput;
 public class TiposPrimitivos {
     public static void main(String[] args) {
         float num = 1825.54f;
-        System.out.printf("O número que você digitou foi %f",num );
+        System.out.printf("O número que você digitou foi %.2f",num );
     }
 }
