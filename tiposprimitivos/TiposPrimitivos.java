d65fc2809cc5cc772fc9c7073405b5567ea8ecb1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 20:18:17 -0300

    formatando print

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -1,8 +1,10 @@
 package tiposprimitivos;
 
+import java.sql.SQLOutput;
+
 public class TiposPrimitivos {
     public static void main(String[] args) {
         float num = 1825.54f;
-        System.out.println("O número que você digitou foi " + num);
+        System.out.printf("O número que você digitou foi %f",num );
     }
 }
