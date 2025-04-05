f4fd4e3cc111a606ddd052b11927f330eeb9223e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 20:09:51 -0300

    criando variável com seu tipo primitivo

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -2,6 +2,7 @@ package tiposprimitivos;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        
+        float num = 1825.54f;
+        System.out.println("O número que você digitou foi " + num);
     }
 }
