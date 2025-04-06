dea1b3e4e62bdfe5567e16993d03bbada0606477
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 22:38:09 -0300

    importando scanner

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -1,12 +1,14 @@
 package tiposprimitivos;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
+        Scanner teclado = Scanner();
         String nome = "Gustavo";
         float num = 1825.54f;
         //System.out.printf("O número que %s digitou foi %.2f",nome,num);
-        System.out.format("Sua nota é %f ",num);
+        System.out.format("A nota de %s é %.2f ",nome,num);
     }
 }
