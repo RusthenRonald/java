855f87d78657f402942ca2a1f4bde0c91433a44a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 23:13:33 -0300

    fazendo conversão

--- testetipos/TesteTipos.java
+++ testetipos/TesteTipos.java
@@ -4,5 +4,7 @@ public class TesteTipos {
     public static void main(String[] args) {
         int idade =30;
         //String valor = (String) idade;  não funciona typecast
+        String valor = Integer.toString(idade);
+        System.out.println(idade);
     }
 }
