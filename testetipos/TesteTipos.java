0a7577bdeba2babf81fd4abbc8ebdecf22b48447
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 23:10:56 -0300

    convertendo int para string

--- testetipos/TesteTipos.java
+++ testetipos/TesteTipos.java
@@ -2,6 +2,7 @@ package testetipos;
 
 public class TesteTipos {
     public static void main(String[] args) {
-
+        int idade =30;
+        //String valor = (String) idade;  não funciona typecast
     }
 }
