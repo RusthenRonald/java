551a182119d13f73906baa47c4c9fa41f65b6b31
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-05 22:25:59 -0300

    usando o format

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -4,7 +4,9 @@ import java.sql.SQLOutput;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
+        String nome = "Gustavo";
         float num = 1825.54f;
-        System.out.printf("O número que você digitou foi %.2f",num );
+        //System.out.printf("O número que %s digitou foi %.2f",nome,num);
+        System.out.format("Sua nota é %f ",num);
     }
 }
