c3588eca39e3594f4a958639f067733691446a14
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-06 23:42:29 -0300

    testando format e conversão entre tipos

--- testetipos/TesteTipos.java
+++ testetipos/TesteTipos.java
@@ -2,13 +2,8 @@ package testetipos;
 
 public class TesteTipos {
     public static void main(String[] args) {
-        /*int idade =30;
-        //String valor = (String) idade;  não funciona typecast
-        String valor = Integer.toString(idade);
-        System.out.println(idade);
-        */
-         String valor = "30.5";
-         float num = Float.parseFloat(valor);
-        System.out.printf("O número convertido é %.3f",num);
+        String num = "30";
+        int valor = Integer.parseInt(num);
+        System.out.printf("O valor digitado foi %d", valor);
     }
 }
