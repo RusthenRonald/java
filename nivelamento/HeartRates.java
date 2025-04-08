e40d5fbc8fe520ca8d627a4ecd92bc44d87e091e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-08 01:35:32 -0300

    organizando código de uma melhor forma

--- nivelamento/HeartRates.java
+++ nivelamento/HeartRates.java
@@ -4,18 +4,12 @@ import java.util.Scanner;
 import java.time.LocalDate;
 
 public class HeartRates {
+    // atributos da pessoa
+    private String nome ;
+    
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Qual seu nome:");
-        String nome = input.nextLine();
-        System.out.println("Qual seu sobrenome:");
-        String sobrenome = input.nextLine();
-        System.out.println("Qual sua data de nascimento");
-        int nasc = input.nextInt();
-        int anoAtual =LocalDate.now().getYear();
-        int idade = anoAtual- nasc;
-        System.out.printf("O nome completo da pessoa é %s %s, sua data de nascimento é %d\n",nome,sobrenome,nasc);
-        System.out.printf("Sua idade é de %d",idade);
+
     }
 
 }
