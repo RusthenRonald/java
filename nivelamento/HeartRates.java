ff31f55398c801776ea9eb57ee87614617ce0aeb
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-08 00:57:03 -0300

    adicionando metodo

---
+++ nivelamento/HeartRates.java
@@ -0,0 +1,12 @@
+package nivelamento;
+
+import java.util.Scanner;
+
+public class HeartRates {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nome = input.nextLine();
+        System.out.println(nome);
+    }
+
+}
--- nivelamento0/HeartRates.java
+++
@@ -1,6 +0,0 @@
-package nivelamento0;
-import java.util.Scanner;
-public class HeartRates {
-    Scanner input = new Scanner(System.in);
-    String nome = input.nextLine();
-}
