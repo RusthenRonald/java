f5500b97f11ad3c0ffb9198a9b257580bb307dfd
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-08 01:05:06 -0300

    exibindo dados

--- nivelamento/HeartRates.java
+++ nivelamento/HeartRates.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class HeartRates {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Qual seu nome:");
         String nome = input.nextLine();
-        System.out.println(nome);
+        System.out.println("Qual seu sobrenome:");
+        String sobrenome = input.nextLine();
+        System.out.println("Qual sua data de nascimento");
+        int nasc = input.nextInt();
+        System.out.printf("O nome completo da pessoa é %s e sua data de nascimento é %d",nome,nasc);
     }
 
 }
