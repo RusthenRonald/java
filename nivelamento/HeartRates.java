632c1ca1f3188fe8ddf48db13dba6a4e730f4cb8
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-08 01:37:19 -0300

    separando atributos

--- nivelamento/HeartRates.java
+++ nivelamento/HeartRates.java
@@ -6,6 +6,10 @@ import java.time.LocalDate;
 public class HeartRates {
     // atributos da pessoa
     private String nome ;
+    private String sobrenome;
+    private int diaNasc;
+    private int mesNasc;
+    private int anoNasc;
     
 
     public static void main(String[] args) {
